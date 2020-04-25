71259b88892fada4f1a1ce1e5c96c4f4f9fd2284
hsj3359 <37091550+hsj3359@users.noreply.github.com>
2020-04-25 14:23:33 +0900

    04/25 4
    4th

--- userdao/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -15,19 +15,22 @@ public class UserDao {
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
 
-        User user;
+        User user = null;
         try {
             connection = dataSource.getConnection();
             preparedStatement = connection.prepareStatement("select id, name, password from userinfo where id = ?");
             preparedStatement.setInt(1, id);
 
             resultSet = preparedStatement.executeQuery();
-            resultSet.next();
+            if( resultSet.next()){
+                user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setName(resultSet.getString("name"));
+                user.setPassword(resultSet.getString("password"));
+            }
 
-            user = new User();
-            user.setId(resultSet.getInt("id"));
-            user.setName(resultSet.getString("name"));
-            user.setPassword(resultSet.getString("password"));
+
+
         } finally {
             try {
                 resultSet.close();
@@ -91,5 +94,60 @@ public class UserDao {
     }
 
 
+    public void update(User user) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = dataSource.getConnection();
 
+            preparedStatement = connection.prepareStatement("update userinfo set name = ?, password = ? where id =?");
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setInt(3, user.getId());
+            preparedStatement.executeUpdate();
+
+        } finally {
+
+            try {
+                preparedStatement.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+
+
+        }
+    }
+
+    public void delete(Integer id) throws SQLException {
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = dataSource.getConnection();
+
+            preparedStatement = connection.prepareStatement("delete from userinfo where id =?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+
+        } finally {
+
+            try {
+                preparedStatement.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+
+
+        }
+    }
 }
