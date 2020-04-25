8ab1486e1527019a0f615bc3024e2606de8630cf
hsj3359 <37091550+hsj3359@users.noreply.github.com>
2020-04-25 13:55:38 +0900

    04/25 2
    second

--- userdao/src/main/java/kr/ac/jejunu/user/DaoFactory.java
+++ userdao/src/main/java/kr/ac/jejunu/user/DaoFactory.java
@@ -1,18 +1,46 @@
 package kr.ac.jejunu.user;
 
+import jdk.nashorn.internal.ir.CallNode;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
 
 public class DaoFactory {
+    @Value("${db.classname}")
+    private String className;
+    @Value("${db.url}")
+    private String url;
+    @Value("${db.username}")
+    private String username;
+    @Value("${db.password}")
+    private String password;
+
+
     @Bean
     public UserDao getUserDao() {
-        return new UserDao(connectionMaker());
+        return new UserDao(dataSource());
     }
-
-
     @Bean
-    public JejuConnectionMaker connectionMaker() {
-
-        return new JejuConnectionMaker();
+    public DataSource dataSource(){
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        try {
+            dataSource.setDriverClass((Class<? extends Driver>) Class.forName(className));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
     }
+
 }
