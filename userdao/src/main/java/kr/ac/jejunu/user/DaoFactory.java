76278bc0e7303412c6f31b2c155474e2d984c6e4
hsj3359 <37091550+hsj3359@users.noreply.github.com>
2020-05-01 18:18:39 +0900

    05/01 5
    5th

--- userdao/src/main/java/kr/ac/jejunu/user/DaoFactory.java
+++ userdao/src/main/java/kr/ac/jejunu/user/DaoFactory.java
@@ -2,6 +2,7 @@ package kr.ac.jejunu.user;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
@@ -24,8 +25,8 @@ public class DaoFactory {
         return new UserDao(jdbcContext());
     }
     @Bean
-    public JdbcContext jdbcContext(){
-        return new JdbcContext(dataSource());
+    public JdbcTemplate jdbcContext(){
+        return new JdbcTemplate(dataSource());
     }
     @Bean
     public DataSource dataSource(){
